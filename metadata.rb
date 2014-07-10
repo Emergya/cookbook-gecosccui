fefdbc423bd85d5f61bc24dfbcf524fd539c9756
Pablo Martín <goinnn@gmail.com>
2014-07-10 13:26:54 +0200

    Ready to new release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sysadmin@yaco.es'
 license          'EUPL V 1.1 http://www.osor.eu/eupl'
 description      'Installs/Configures gecoscc-ui'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5'
+version          '0.6'
 
 depends 'python'
 depends 'build-essential'
